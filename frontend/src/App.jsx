55e7cf47628d61f06ef0728f49c33ee6e726c9ca
gsanz <gorsanmo@gmail.com>
2026-05-18 20:54:57 +0200

    navbar updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Tasks from './pages/Tasks';
-import CreateTask from './pages/CreateTask';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/create" element={<CreateTask />} />
-      </Routes>
-    </BrowserRouter>
+      <><Navbar /><Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/tasks" element={<Tasks />} />
+      </Routes></>
   );
 }
