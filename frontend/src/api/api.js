2de94b2493688f786f22cca5fe0cbe73cfd2a266
gsanz <gorsanmo@gmail.com>
2026-05-18 23:52:44 +0200

    frontend docker file changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
+/*const api = axios.create({
   baseURL: 'http://localhost:3000/api',
+});*/
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 🔐 attach token automatically
